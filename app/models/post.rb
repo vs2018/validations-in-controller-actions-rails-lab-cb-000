082667dd57fc5750211d15776e73c62ce85c5d77
Vishal Sharma <vsh1819@gmail.com>
2018-07-16 19:08:35 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
    validates :title, presence: true
   validates :email, uniqueness: true
+         validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
   
 end
